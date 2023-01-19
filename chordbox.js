381929bf5415967eac37231be964599dfb883a04
Jakub Neumann <4jakub.neumann7@gmail.com>
2023-01-19 10:14:18 +0100

    spacing fix

--- chordbox.js
+++ chordbox.js
@@ -64,7 +64,7 @@ class ChordBox {
 
     // Add room on sides for finger positions on 1. and 6. string
     // this.x = this.params.x + this.params.width * 0.15 + this.spacing;
-    this.x = this.params.x;
+    this.x = this.params.x + this.width / 40 + this.spacing;
     this.y = this.params.y + this.params.height * 0.15 + this.fretSpacing;
 
     this.metrics = {
